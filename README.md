f9762c6098b4561919d6bc8468fe583438b10653
NICO LUEG <nico.lueg@mercedes-benz.com>
2023-05-23 14:07:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# eslint-config-nlueg-angular
+
+[![GitHub License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/NLueg/eslint-config-nlueg-angular/blob/master/LICENSE)
+[![NPM version](https://img.shields.io/npm/v/eslint-config-nlueg-angular.svg)](https://www.npmjs.com/package/eslint-config-nlueg-angular)
+
+This package includes a basic ESLint configuration for linting and formatting my Angular code.
+
+As a basis I use [Angular ESLint](https://github.com/angular-eslint/angular-eslint), [TypeScript ESLint](https://github.com/typescript-eslint/typescript-eslint), [eslint-config-prettier](https://github.com/prettier/eslint-config-prettier) and [eslint-plugin-rxjs](https://github.com/cartant/eslint-plugin-rxjs).
+Also I include [eslint-plugin-import](https://github.com/import-js/eslint-plugin-import) and [eslint-plugin-unused-imports](https://github.com/sweepline/eslint-plugin-unused-imports) to have beautiful imports.
+
+I use this config basically in every Angular-Project to have less smelly and well formatted code ✨
+
---
+++ index.js
@@ -0,0 +1,127 @@
+module.exports = {
+    "root": true,
+    "ignorePatterns": [
+        "projects/**/*"
+    ],
+    "overrides": [
+        {
+            "files": [
+                "*.ts"
+            ],
+            "parser": "@typescript-eslint/parser",
+            "parserOptions": {
+                "project": [
+                    "tsconfig.json",
+                    "e2e/tsconfig.json"
+                ],
+                "createDefaultProgram": true
+            },
+            "plugins": [
+                "@typescript-eslint",
+                "simple-import-sort",
+                "unused-imports",
+                "import",
+                "prettier",
+                "rxjs",
+                "rxjs-angular"
+            ],
+            "extends": [
+                "eslint:recommended",
+                "plugin:@typescript-eslint/eslint-recommended",
+                "plugin:@typescript-eslint/recommended",
+                "plugin:@angular-eslint/recommended",
+                "plugin:@angular-eslint/template/process-inline-templates",
+                "plugin:prettier/recommended",
+                "plugin:rxjs/recommended"
+            ],
+            "rules": {
+                "prettier/prettier": [
+                    "error",
+                    {
+                        "singleQuote": true,
+                        "useTabs": false,
+                        "tabWidth": 2,
+                        "semi": true,
+                        "bracketSpacing": true,
+                        "endOfLine": "auto"
+                    }
+                ],
+                "rxjs/no-implicit-any-catch": "off",
+                "rxjs/no-ignored-observable": "error",
+                "rxjs/no-sharereplay": "off",
+                "rxjs/throw-error": "error",
+                "simple-import-sort/imports": "error",
+                "simple-import-sort/exports": "error",
+                "import/first": "error",
+                "import/newline-after-import": "error",
+                "import/no-duplicates": "error",
+                "@angular-eslint/component-selector": [
+                    "error",
+                    {
+                        "prefix": "app",
+                        "style": "kebab-case",
+                        "type": "element"
+                    }
+                ],
+                "@angular-eslint/directive-selector": [
+                    "error",
+                    {
+                        "prefix": "app",
+                        "style": "camelCase",
+                        "type": "attribute"
+                    }
+                ],
+                "@angular-eslint/component-class-suffix": [
+                    "error",
+                    {
+                        "suffixes": [
+                            "Component",
+                            "Page"
+                        ]
+                    }
+                ],
+                "@typescript-eslint/no-explicit-any": "off",
+                "@typescript-eslint/no-empty-function": "off",
+                "@typescript-eslint/await-thenable": "error",
+                "@typescript-eslint/explicit-module-boundary-types": "error",
+                "unused-imports/no-unused-imports": "error",
+                "no-unused-vars": "off",
+                "@typescript-eslint/no-unused-vars": "off",
+                "unused-imports/no-unused-vars": [
+                    "warn",
+                    {
+                        "vars": "all",
+                        "varsIgnorePattern": "^_",
+                        "args": "after-used",
+                        "argsIgnorePattern": "^_"
+                    }
+                ],
+                "@typescript-eslint/no-non-null-assertion": "off"
+            }
+        },
+        {
+            "files": [
+                "*.html"
+            ],
+            "plugins": ["prettier"],
+            "extends": [
+                "plugin:@angular-eslint/template/recommended",
+                "plugin:prettier/recommended"
+            ],
+            "rules": {
+                "prettier/prettier": [
+                    "error",
+                    {
+                        "singleQuote": true,
+                        "useTabs": false,
+                        "tabWidth": 2,
+                        "semi": true,
+                        "bracketSpacing": true,
+                        "endOfLine": "auto",
+                        "parser": "angular"
+                    }
+                ]
+            }
+        }
+    ]
+}
